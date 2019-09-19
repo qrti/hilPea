351d1c77584661ff08d043d1ad1436b29cb3f9e3
Kurt Schuster <qrt@qland.de>
2019-09-19 10:56:46 +0200

    rework

--- README.md
+++ README.md
@@ -1,33 +1,31 @@
 # **Hilbert + Peano + 2D + 3D + Python + Script + Blender**
 
 ## **Hilbert**
-![Image](images/hibert2d_3s.png)  
-hilbert 2d, iteration 3
 
-![Image](images/hilbert3d_2rs.png)  
-hilbert 3d, iteration 2
+![Image](images/hilbert2d_var0.png)  
+hilbert 2d, variation 0, iteration 1, 8 possibilities
 
-![Image](images/hilbert3d_3rs.png)  
-hilbert 3d, iteration 3
+![Image](images/hilbert2d_1_4.png)  
+hilbert 2d, variation 0, iteration 1-4
+
+![Image](images/hilbert3d_var0.png)  
+hilbert 3d, variation 0, iteration 1, 48 possibilities
+
+![Image](images/hilbert3d_1_4r.png)  
+hilbert 3d, variation 0, iteration 1-4
 
 ## **Peano**
-![Image](images/peano2d_var0s.png)  
+![Image](images/peano2d_var0.png)  
 peano 2d, variation 0, iteration 1, 8 possibilities
 
-![Image](images/peano2d_3s.png)  
-peano 2d, variation 0, iteration 3
+![Image](images/peano2d_1_3.png)  
+peano 2d, variation 0, iteration 1-3
 
-![Image](images/peano3d_2rs.png)  
-peano 3d, variation 0, iteration 2
-
-![Image](images/peano3d_3rs.png)  
-peano 3d, variation 0, iteration 3
-
-![Image](images/peano3d_var0s.png)  
+![Image](images/peano3d_var0.png)  
 peano 3d, variation 0, iteration 1, 48 possibilities
 
 ## **Remarks**
-larger images in image drawer
+some larger images in image/large
 
 ## **Versions**
 \- 0.9 initial  
--- source/hilPea.py
+++ source/hilPea.py
@@ -101,10 +101,13 @@ def hilbert2d(s, x, y, a, b):
 
     s >>= 1
 
-    hilbert2d(s, x+s*a,     y+s*a,       a, 1-b)
-    hilbert2d(s, x+s*b,     y+s*(1-b),   a,   b)
-    hilbert2d(s, x+s*(1-a), y+s*(1-a),   a,   b)
-    hilbert2d(s, x+s*(1-b), y+s*b,     1-a,   b)
+    x, y, _ = a.cor(s, x, y)           
+    x, y, _ = b.cor(s, x, y)
+
+    hilbert2d(s, x,             y,              b,  a)
+    hilbert2d(s, x+s*b.x,       y+s*b.y,        a,  b)
+    hilbert2d(s, x+s*(a.x+b.x), y+s*(a.y+b.y),  a,  b)
+    hilbert2d(s, x+s*a.x,       y+s*a.y,       -b, -a)
 
 def hilbert3d(s, x, y, z, a, b, c):
     if s == 1:
@@ -187,6 +190,7 @@ def peano3d(s, x, y, z, a, b, c):
     peano3d(s, x+s*(a.x+2*b.x+2*c.x), y+s*(a.y+2*b.y+2*c.y), z+s*(a.z+2*b.z+2*c.z),  a, -b, -c)
     peano3d(s, x+s*2*(a.x+b.x+c.x),   y+s*2*(a.y+b.y+c.y),   z+s*2*(a.z+b.z+c.z),    b,  a,  c)
 
+
 def createMeshFromData(name, origin):
     me = bpy.data.meshes.new(name + 'Mesh')     # create mesh and object
     ob = bpy.data.objects.new(name, me)
@@ -203,13 +207,13 @@ def createMeshFromData(name, origin):
 
 
 name = "hilbert"
-# hilbert2d(2, 0, 0, 0, 0)
-hilbert3d(4, 0, 0, 0, L(1, 0, 0), L(0, 1, 0), L(0, 0, 1))
-
+hilbert2d(8, 0, 0, L(1, 0), L(0, 1))
+# hilbert3d(16, 0, 0, 0, L(1, 0, 0), L(0, 1, 0), L(0, 0, 1))
+#
 # name = "peano"
 # peano2d(27, 0, 0, L(1, 0), L(0, 1))	
 # peano3d(3, 0, 0, 0, L(1, 0, 0), L(0, 1, 0), L(0, 0, 1))
-
+#
 cleanEdges()
 createMeshFromData(name, (0, 0, 0))
 
@@ -239,7 +243,9 @@ createMeshFromData(name, (0, 0, 0))
 #     q = (q * q) / (d * e)
 # 
 #     return q >= 1.0 - 1E-10
-    
+
+# mark start points of curves
+#     
 # def markStart():
 #     global verts, edges
 # 
@@ -255,25 +261,34 @@ createMeshFromData(name, (0, 0, 0))
 #     verts += [(verts[0][0], verts[0][1], verts[0][2]+.2)]
 #     edges += [(len(verts)-2, len(verts)-1)]
 
-# list variations peano 2d
+# list variations hilbert/peano 2d
 #
-# dir = { '+a': L(1, 0), '+b': L(0, 1), '-a': -L(1, 0), '-b': -L(0, 1) }
+# dir = { 'a': L(1, 0), 'b': L(0, 1) }
 # x = -3 * 4
 # 
-# for i in sorted(dir):    
-#     for j in sorted(dir):
-#         if(i[-1] != j[-1]):
-#             verts = []
-#             edges = []
+# for s in range(4):
+#     for i in sorted(dir):    
+#         for j in sorted(dir):
+#             if(i[-1] != j[-1]):
+#                 verts = []
+#                 edges = []
 # 
-#             name = "{}{}".format(i, j)
-#             peano2d(3, 0, 0, dir[i], dir[j])
+#                 si = '-' if i=='a' and s&2 or i=='b' and s&1 else '+'
+#                 sj = '-' if j=='a' and s&2 or j=='b' and s&1 else '+'
 # 
-#             markStart()
-#             createMeshFromData(name, (x, 0, 0))
-#             x += 3
+#                 name = "{}{}".format(si+i, sj+j)
+# 
+#                 a = dir[i] if si=='+' else -dir[i]
+#                 b = dir[j] if sj=='+' else -dir[j]
+# 
+#                 # hilbert2d(2, 0, 0, a, b)
+#                 peano2d(3, 0, 0, a, b)
+# 
+#                 markStart()
+#                 createMeshFromData(name, (x, 0, 0))
+#                 x += 3
 
-# list variations peano 3d
+# list variations hilbert/peano 3d
 #
 # dir = { 'a': L(1, 0, 0), 'b': L(0, 1, 0), 'c': L(0, 0, 1) }
 # 
@@ -298,7 +313,8 @@ createMeshFromData(name, (0, 0, 0))
 #                     b = dir[j] if sj=='+' else -dir[j]
 #                     c = dir[k] if sk=='+' else -dir[k]
 # 
-#                     peano3d(3, 0, 0, 0, a, b, c)
+#                     hilbert3d(2, 0, 0, 0, a, b, c)
+#                     # peano3d(3, 0, 0, 0, a, b, c)
 # 
 #                     markStart()
 #                     createMeshFromData(name, (x*4, y*4, 0))
@@ -329,7 +345,21 @@ createMeshFromData(name, (0, 0, 0))
 #     lc = Triple(x, y, z)
 #     c0 = c1
 
-# arbitrary peano 2d version
+# hilbert 2d
+# 
+# def hilbert2d(s, x, y, a, b):
+#     if s == 1:
+#         draw(x, y, 0)
+#         return
+# 
+#     s >>= 1
+# 
+#     hilbert2d(s, x+s*a,     y+s*a,       a, 1-b)
+#     hilbert2d(s, x+s*b,     y+s*(1-b),   a,   b)
+#     hilbert2d(s, x+s*(1-a), y+s*(1-a),   a,   b)
+#     hilbert2d(s, x+s*(1-b), y+s*b,     1-a,   b)
+
+# peano 2d, variation
 #
 # peano2d(3, 0, 0, 0, 0)	
 # 
